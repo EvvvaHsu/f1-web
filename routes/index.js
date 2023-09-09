717519c11c60ac1ca7e454365b1db6951c3ee155
yiching-hsu <ychsu7149@gmail.com>
2023-09-09 23:36:00 +0800

    add forgot password page

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 router.get('/signup', (req, res) => res.render('signup'))
 router.get('/signin', (req, res) => res.render('signin'))
+router.get('/forgotpassword', (req, res) => res.render('forgotpassword'))
 
 router.get('/', (req, res) => res.render('homepage'))
 
