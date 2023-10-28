ed05538fdb065d8c64e0cf14718b129cb57ab8b0
yiching-hsu <ychsu7149@gmail.com>
2023-10-28 15:57:22 +0800

    add pagination

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,4 +1,5 @@
 const { Product } = require('../models')
+const { getOffset, getPagination } = require('../helpers/pagination-helper')
 
 const productController = {
   getHomePage: async (req, res, next) => {
@@ -17,13 +18,21 @@ const productController = {
   },
   getCateprod: async (req, res, next) => {
     try {
-      const benzproducts = await Product.findAll({
+      const DEFAULT_LIMIT = 4
+      const page = Number(req.query.page) || 1
+      const limit = Number(req.query.limit) || DEFAULT_LIMIT
+      const offset = getOffset(limit, page)
+
+      const benzproducts = await Product.findAndCountAll({
         order: [
           ['createdAt', 'DESC']
-        ]
+        ],
+        limit,
+        offset,
+        nest: true
       })
-      const plainBenzproducts = benzproducts.map(product => product.get({ plain: true }))
-      res.render('cateprod', { benzproducts: plainBenzproducts })
+      const plainBenzproducts = benzproducts.rows.map(product => product.get({ plain: true }))
+      await res.render('cateprod', { benzproducts: plainBenzproducts, pagination: getPagination(limit, page, benzproducts.count) })
     } catch (err) {
       return next(err)
     }
