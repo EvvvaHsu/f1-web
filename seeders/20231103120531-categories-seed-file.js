41ee04238141b899056dca35767dab07f4ec230e
yiching-hsu <ychsu7149@gmail.com>
2023-11-03 20:17:00 +0800

    add categories seeder

---
+++ seeders/20231103120531-categories-seed-file.js
@@ -0,0 +1,18 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Categories', ['Mercedes', 'Haas', 'Red bull', 'Alpine', 'McLaren', 'Ferrari', 'Alfa Romeo', 'Alpha Tauri', 'Williams', 'Aston Martin', 'Lewis Hamilton', 'George Russell', 'Max Verstappen', 'Sergio Perez', 'Charles Leclerc', 'Carlos Sainz', 'Lando Norris', 'Oscar Piastri', 'Yuki Tsunoda', 'Daniel Ricciardo', 'Esteban Ocon', 'Pierre Gasly', 'Valtteri Bottas', 'Zhou Guanyu', 'Lance Stroll', 'Fernando Alonso', 'Nico Hulkenberg', 'Kevin Magnussen', 'Alexander Albon', 'Logan Sargeant', 'Men', 'Women', 'Kids', 'Others'].map(item => {
+      return {
+        name: item,
+        created_at: new Date(),
+        updated_at: new Date()
+      }
+    }), {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Categories', {})
+  }
+}
