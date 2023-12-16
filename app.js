389efaa4cd64acc4553a85c373a957663163dc9a
yiching-hsu <ychsu7149@gmail.com>
2023-12-16 16:37:54 +0800

    cannot find js

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use(express.static('public'))
 
 app.use(methodOverride('_method'))
 app.use('/upload', express.static(path.join(__dirname, 'upload')))
+app.use('/helpers', express.static(path.join(__dirname, 'helpers')))
 
 app.use(flash())
 
