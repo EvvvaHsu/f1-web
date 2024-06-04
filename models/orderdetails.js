d571b33ee3e9f2b89aeeee405ed365fc7212f7fb
yiching-hsu <ychsu7149@gmail.com>
2024-06-04 18:38:21 +0800

    modification

--- models/orderdetails.js
+++ models/orderdetails.js
@@ -1,7 +1,7 @@
-'use strict';
+'use strict'
 const {
   Model
-} = require('sequelize');
+} = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class Orderdetails extends Model {
     /**
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate (models) {
       // define association here
     }
   }
@@ -27,7 +27,8 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Orderdetails',
-    underscored: true,
-  });
-  return Orderdetails;
-};
+    tableName: 'Orderdetails',
+    underscored: true
+  })
+  return Orderdetails
+}
